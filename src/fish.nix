0e884df0cbbe7c4aeb9b371708eb2eda9f9a4c1f
Mayeul d'Avezac <1085775-mdavezac@users.noreply.gitlab.com>
2021-01-10 15:46:58 +0000

    update fish setup

--- src/fish.nix
+++ src/fish.nix
@@ -1,9 +1,8 @@
 { config, pkgs, lib, ... }: {
+  home.packages = [ pkgs.fish-foreign-env ];
   programs.fish = {
     enable = true;
     shellAliases = {
-      issues = "${pkgs.gitAndTools.hub}/bin/hub browse -- issues";
-      pulls = "${pkgs.gitAndTools.hub}/bin/hub browse -- pulls";
       cat = "${pkgs.bat}/bin/bat";
       vi = "nvim";
       vim = "nvim";
@@ -15,27 +14,16 @@
     };
     interactiveShellInit = ''
       ${pkgs.any-nix-shell}/bin/any-nix-shell fish | source
-      # because liluball/nix-env keeps growing the path
-      set -gx NIX_PATH (printf "%s\n" $NIX_PATH | uniq)
 
       set -U fish_user_path /etc/profiles/per-user/mdavezac/bin
 
       ${pkgs.python3Packages.pip}/bin/pip completion --fish | source
+      fenv source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
     '';
-    plugins = [
-      {
-        name = "nix-env";
-        src = pkgs.fetchFromGitHub {
-          owner = "lilyball";
-          repo = "nix-env.fish";
-          rev = "cf99a2e6e8f4ba864e70cf286f609d2cd7645263";
-          sha256 = "0170c7yy6givwd0nylqkdj7kds828a79jkw77qwi4zzvbby4yf51";
-        };
-      }
-    ];
   };
   programs.zoxide = {
     enable = true;
     enableFishIntegration = true;
   };
+  programs.command-not-found.enable = true;
 }
