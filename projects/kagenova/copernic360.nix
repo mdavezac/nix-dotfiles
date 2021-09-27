855683730b7c002a74a784e321f7401d96df351f
Mayeul d'Avezac <1085775-mdavezac@users.noreply.gitlab.com>
2021-09-27 10:39:33 +0100

    Update copernic360 switch_stack to have local dev

--- projects/kagenova/copernic360.nix
+++ projects/kagenova/copernic360.nix
@@ -125,6 +125,15 @@ in
     };
     file.".local/bin/switch_stack.fish".text = ''
       function switch_stack -a arg
+          set -e AWS_PROFILE
+          set -e SQL_PASSWORD
+          set -e SQL_USERNAME
+          set -e SQL_PORT
+          set -e GOOGLE_APPLICATION_CREDENTIALS
+          set -e COPERNIC360_HOST
+          set -e PYTEST_PULUMI_STACK
+          set -e AWS_REGION
+          set -gx SQL_DATABASE spatial360_api
           set local_pwd "/Users/mdavezac/kagenova/ai-pipeline/"
           set stack $arg
           switch $arg
@@ -162,20 +171,24 @@ in
                   set gcp_config production
                   set aws_profile production
                   set puldir $local_pwd/infrastructure
+              case local
+                  set -e GOOGLE_APPLICATION_CREDENTIALS
+                  set -gx PYTEST_PULUMI_STACK local
+                  set -gx SQL_PASSWORD "p@ssw0rd1"
+                  set -gx SQL_USERNAME root
+                  set -gx SQL_PORT 3306
+                  return 0
               case '*'
                   echo not a known stack $stack
                   return 1
           end
-          if string match -qer "(bootstrap|monitoring).*" "$stack"
-              set -e GOOGLE_APPLICATION_CREDENTIALS
-              set -e COPERNIC360_HOST
-          else
+          set -gx AWS_PROFILE $aws_profile
+          pulumi -C $puldir stack select $stack
+          if ! string match -qer "(bootstrap|monitoring).*" "$stack"
               set -gx GOOGLE_APPLICATION_CREDENTIALS $local_pwd/.local/spatial360-$gcp_config.json
               set -gx COPERNIC360_HOST (pulumi stack output -C $local_pwd/infrastructure engine-url)
           end
-          set -gx AWS_PROFILE $aws_profile
           gcloud config configurations activate copernic360-$gcp_config
-          pulumi -C $puldir stack select $stack
       end
     '';
   };
