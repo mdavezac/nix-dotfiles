9966c89a312ee69ca3ce25edfb0abcf89c4a946e
Mayeul d'Avezac <1085775-mdavezac@users.noreply.gitlab.com>
2022-04-21 11:14:04 +0100

    Formatting

--- projects/lib/envrc.nix
+++ projects/lib/envrc.nix
@@ -3,7 +3,7 @@ let
   emptyAttrs = attrs: builtins.length (builtins.attrNames attrs) != 0;
   leftOrRight = left: right: if builtins.isNull left then right else left;
   cloner = name:
-  { url, dest, origin, ignore, settings }:
+    { url, dest, origin, ignore, settings }:
     let
       destination = leftOrRight dest name;
       createSettings = settings:
@@ -12,27 +12,27 @@ let
           text = lib.generators.toGitINI settings;
         };
     in
-      ''
-        [ -d "${destination}/.git/objects" ] || cloner ${url} ${destination} ${origin}
-      '' + lib.optionalString (emptyAttrs settings) ''
-        git_settings ${createSettings settings} ${destination}
-      '' + lib.optionalString (lib.stringLength ignore > 0) ''
-        mkdir -p ${destination}/.git/info
-        cat > ${destination}/.git/info/exclude <<EOF
-        ${ignore}
-        EOF
-      '';
+    ''
+      [ -d "${destination}/.git/objects" ] || cloner ${url} ${destination} ${origin}
+    '' + lib.optionalString (emptyAttrs settings) ''
+      git_settings ${createSettings settings} ${destination}
+    '' + lib.optionalString (lib.stringLength ignore > 0) ''
+      mkdir -p ${destination}/.git/info
+      cat > ${destination}/.git/info/exclude <<EOF
+      ${ignore}
+      EOF
+    '';
   cloners = repos:
     (builtins.concatStringsSep "\n" (lib.mapAttrsToList cloner repos));
   isNixshellEnabled = (import ./nixshell.nix).isEnabled;
 
 in
 (cloners config.repos) + ''
-  export fish_history="${
-builtins.replaceStrings [ "-" " " ] [ "_" "_" ]
-  (builtins.concatStringsSep "" project)
-}"
-  export TMUX_SESSION_NAME=${builtins.concatStringsSep "-" project}
+    export fish_history="${
+  builtins.replaceStrings [ "-" " " ] [ "_" "_" ]
+    (builtins.concatStringsSep "" project)
+  }"
+    export TMUX_SESSION_NAME=${builtins.concatStringsSep "-" project}
 '' + lib.optionalString (isNixshellEnabled config.nixshell) ''
   eval "$(lorri direnv)"
 '' + lib.optionalString (lib.stringLength config.vim > 0) ''
